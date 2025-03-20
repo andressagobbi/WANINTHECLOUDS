6bd45efbd24e4080883951b2641a7c80c7dda20d
andressagobbi <andressabgobbi@gmail.com>
2025-03-20 20:30:11 -0300

    Initial commit

---
+++ code/Level.py
@@ -0,0 +1,16 @@
+#!usr/bin/python
+#-*- Coding: uft-8 -*-
+
+new*
+class Level:
+    new *
+    def __init__(self):
+        self.window  = None
+        self.name = None
+        self.entity.list = None
+        self.entity.list = None
+
+
+    new*
+    def run (self, ):
+        pass
---
+++ code/Player.py
@@ -0,0 +1,20 @@
+#!usr/bin/python
+#-*- Coding: uft-8 -*-
+
+from Entify import Entify
+
+
+new *
+
+class Player (Entify):
+    new*
+    def __init__(self):
+        pass
+
+    new*
+    def update (self, ):
+        pass
+
+    new*
+    def move(self, ):
+        pass
