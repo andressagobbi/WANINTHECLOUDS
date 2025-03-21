95a9032390c5aedabd27904b75b55592bed74bb8
andressagobbi <andressabgobbi@gmail.com>
2025-03-20 22:13:02 -0300

    Created assed folder Interting all assets creating the menu: background, sound and fonts creating the const file

--- code/Menu.py
+++ code/Menu.py
@@ -5,28 +5,29 @@ import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_WIDTH, C_ORANGE, MENU_OPTION, C_WHITE, C_YELLOW
+from code.const import WIN_WIDTH, C_PURPLE, C_WHITE, MENU_OPTION
 
 
 class Menu:
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load('./asset/MenuBg.png').convert_alpha()
+        self.surf = pygame.image.load('./asset/menu.png').convert_alpha()
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self):
         menu_option = 0
-        pygame.mixer_music.load('./asset/Menu.mp3')
+        pygame.mixer_music.load('./assent/menu.wav')
         pygame.mixer_music.play(-1)
+
         while True:
             # DRAW IMAGES
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, "Mountain", C_ORANGE, ((WIN_WIDTH / 2), 70))
-            self.menu_text(50, "Shooter", C_ORANGE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(30, "War in the", C_PURPLE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Clouds", C_PURPLE, ((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
                 if i == menu_option:
-                    self.menu_text(20, MENU_OPTION[i], C_YELLOW, ((WIN_WIDTH / 2), 200 + 25 * i))
+                    self.menu_text(20, MENU_OPTION[i], C_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
                 else:
                     self.menu_text(20, MENU_OPTION[i], C_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
             pygame.display.flip()
---
+++ code/const.py
@@ -0,0 +1,15 @@
+#C
+C_PURPLE = (58, 8, 62)
+C_WHITE = (255, 255, 255)
+
+#M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+
+#W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
